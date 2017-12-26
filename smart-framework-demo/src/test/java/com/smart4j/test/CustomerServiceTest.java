4a5a71a6814414fc6f7020504b6615e1e7ad221f
liugaowei999 <187346454@qq.com>
2017-12-26 17:06:54 +0800

    666666

---
+++ smart-framework-demo/src/test/java/com/smart4j/test/CustomerServiceTest.java
@@ -0,0 +1,36 @@
+package com.smart4j.test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+
+import com.smart4j.helper.DatabaseHelper;
+import com.smart4j.utils.ClassUtil;
+
+//import com.liugw.learn.helper.DatabaseHelper;
+
+public class CustomerServiceTest {
+
+	@Test
+	public void init() throws Exception {
+		String file = "sql/customer_init.sql";
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(file);
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+		String sql;
+		while ((sql = reader.readLine()) != null) {
+			System.out.println(sql);
+			DatabaseHelper.executeUpdate(sql);
+		}
+
+	}
+
+	@Test
+	public void testClassUtil() {
+		ClassUtil.getClassSet("com.liugw.learn.pojo");
+		ClassUtil.getClassSet("org.junit");
+	}
+}
---
+++ smart-framework-demo/src/test/resources/sql/customer_init.sql
@@ -0,0 +1,3 @@
+truncate customer;
+insert into customer(name, contact, telephone, email, remark) values ('customer3', 'david','1234098','david@qq.com',null);
+insert into customer(name, contact, telephone, email, remark) values ('customer5', 'david','66666','jack@qq.com',null);
